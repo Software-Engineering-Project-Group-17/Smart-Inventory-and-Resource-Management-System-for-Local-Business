da3a4cc754294357c6c6a2b1246ffb451ea75fc9
nimashXDilshan <162735167+nimashXDilshan@users.noreply.github.com>
2025-07-21 22:04:02 +0530

    add some services to the backend

---
+++ backend/Notification_service/Notification_service/src/test/java/com/project_07/Notification_service/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project_07.Notification_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
