1657a72f98a67f5547bea450dafa4f13d01253f0
thivinu22 <thivinu.22@cse.mrt.ac.lk>
2025-09-06 11:14:23 +0530

    add item done

--- frontend/src/app/(site)/inventory/page.tsx
+++ frontend/src/app/(site)/inventory/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import { RefreshCw, AlertTriangle } from "lucide-react";
+import { RefreshCw, AlertTriangle, Plus } from "lucide-react";
 import { withAuth } from "@/hooks/useAuth";
 import { useInventoryData } from "@/hooks/useInventoryData";
 import { InventoryTable } from "@/components/inventory/InventoryList";
@@ -84,14 +84,10 @@ const InventoryPage = () => {
                   Add Category
                 </Link>
                 <Link
-                  href="/"
+                  href="/inventory/addItem"
                   className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-[#3674B5] hover:bg-blue-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 disabled:opacity-50"
                 >
-                  <RefreshCw
-                    className={`h-4 w-4 mr-2 ${
-                      isLoading ? "animate-spin" : ""
-                    }`}
-                  />
+                  <Plus className="h-4 w-4 mr-2" />
                   Add Item
                 </Link>
                 <Link
